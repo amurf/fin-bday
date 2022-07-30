f6cd52083573974f973e7749fce0aa44b5834b98
ash <ash@strategicdata.com.au>
2022-07-30 19:29:42 +1000

    refactor: just serve from root dir

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true src/index.html > dist/index.html
+npm install -g html-minifier
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true index-src.html > index.html
